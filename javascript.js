43b5b7d1079428a4033093e470d9a63114545500
Fudge-Lickels <107988982+Fudge-Lickels@users.noreply.github.com>
2024-05-20 18:44:07 +0200

    Logic defined. Not sure this is right.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,16 @@
 
 
+function get_random_system_outcome() {
+    const options = ["Rock", "Paper", "Scissors"];
+    const randomIndex = Math.floor(Math.random() * options.length);
+    return options[randomIndex];
+}
+function is_round_winner_player(player, csystem) {
+    return (
+        (player === "rock" && csystemsystem === "scissors") ||
+        (player === "scissors" && csystemsystem === "paper") ||
+        (player === "paper" && csystem === "rock")
+    );
+}
+
 
